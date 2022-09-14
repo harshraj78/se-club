fd7af10829cc8b5eb89127b5b5f428818de3d7aa
Akanksha Kushwaha <cmakanksha@gmail.com>
2022-09-14 09:17:13 +0530

    Feat: updated url

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,13 +6,13 @@ function Social() {
     <div id = 'social-page' className='social'>
       <h3>Find Software Engineering Club</h3>
       <div className="social-media d-flex flex-row justify-content-center flex-wrap">
-        <a href="https://www.linkedin.com/" target="_blank">
+        <a href="https://www.linkedin.com/company/amity-seclub" target="_blank">
           <div className="icon fab fa-linkedin-in"></div>
         </a>
-        <a href="https://www.instagram.com/" target="_blank">
+        <a href="https://www.instagram.com/se.club_/" target="_blank">
           <div className="icon fab fa-instagram"></div>
         </a>
-        <a href="https://facebook.com/" target="_blank">
+        <a href="https://www.facebook.com/asecnoida" target="_blank">
           <div className="icon fab fa-facebook-square"></div>
         </a>
       </div>
